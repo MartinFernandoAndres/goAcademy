47879550a3a688fac4259e35460fa9d8c905a7c0
joka828 <joaquin.candalft@gmail.com>
2017-11-24 15:31:54 -0300

    la fecha no funca

--- src/domain/domain_manager.go
+++ src/domain/domain_manager.go
@@ -1,11 +1,14 @@
 package domain
 
+import "time"
+
 type Tweet struct {
 	User string
 	Text string
+	Date time.Date
 }
 
 func NewTweet(userIn, textIn string) *Tweet {
-	tweet := Tweet{userIn, textIn}
+	tweet := Tweet{userIn, textIn, time.Now().Date}
 	return &tweet
 }
--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -19,10 +19,18 @@ func TestPublishedTweetIsSaved(t *testing.T) {
 
 	publishedTweet := service.GetTweet()
 
+	// if time.Now().Equal {
+	// 	println("asd")
+	// }
+
 	if publishedTweet.User != user &&
 		publishedTweet.Text != text {
 		t.Errorf("Expected tweet is %s: %s \nbut is %s: %s",
 			user, text, publishedTweet.User, publishedTweet.Text)
 	}
+
+	// if publishedTweet.Date == nil {
+	// 	t.Error("expecterd dat can't be nil")
+	// }
 
 }
